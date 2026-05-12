4977a8efac4dec30fd33604f382fa3633d12f32f
JaImes <duongquockhanh230596@gmail.com>
2026-05-12 07:54:55 +0700

    lecture 03: splitting func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,16 +19,18 @@ func main() {
 	r := gin.Default()
 
 	// Define a simple GET endpoint
-	r.GET("/ping", func(c *gin.Context) {
-		// Return JSON response
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", Pong)
 
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
 	if err := r.Run(); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
+}
+
+func Pong(c *gin.Context) {
+	// Return JSON response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
